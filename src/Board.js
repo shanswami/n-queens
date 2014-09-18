4baae30689573731d55ce79b4fb9ab2464ca68b7
shanswami <shan.swami@gmail.com>
2014-09-18 16:44:58 -0700

    Complete N queens basic requirements

--- src/Board.js
+++ src/Board.js
@@ -8,9 +8,9 @@
 
     initialize: function (params) {
       if (_.isUndefined(params) || _.isNull(params)) {
-        // console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
-        // console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;','color: blue;', 'color: black;', 'color: grey;');
-        // console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;','color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
+        console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
+        console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;','color: blue;', 'color: black;', 'color: grey;');
+        console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;','color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
       } else if (params.hasOwnProperty('n')) {
         this.set(makeEmptyMatrix(this.get('n')));
       } else {
@@ -126,7 +126,7 @@
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
       for(var i = 0; i < this.get(0).length; i++) {
-        if(this.hasColConflictAt(this.get(0)[i])) {
+        if(this.hasColConflictAt(i)) {
           return true;
         }
       }
--- src/solvers.js
+++ src/solvers.js
@@ -13,38 +13,109 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+window.findNRooksSolution = function(N) {
 
-  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var newBoard = new Board({n:N});
+  var allRowCombos = [];
+  for(var i = 0; i < N; i++) {
+    rowCombo = [];
+    for(var j = 0; j < N; j++) {
+      rowCombo.push(0);
+    }
+  rowCombo[i] = 1;
+  allRowCombos.push(rowCombo);
+  }
+
+
+  for(var rowCounter = 0; rowCounter < N; rowCounter++ ) {
+    for(var rowComboIndex = 0; rowComboIndex < allRowCombos.length; rowComboIndex++) {
+      newBoard.set(rowCounter, allRowCombos[rowComboIndex]);
+      if(!newBoard.hasAnyColConflicts()) {
+        break;
+      }
+    }
+  }
+  if(newBoard.hasAnyColConflicts()) {
+    return null;
+  } else {
+    return newBoard.rows();
+  }
+
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+window.countNRooksSolutions = function(N) {
+  var solutionCount = 0;
+  var board = new Board({n:N})
+  var findSolution = function(row) {
+    if(row === N){
+      solutionCount++;
+      return;
+    }
+    for(var i = 0; i < N; i++){
+      board.togglePiece(row,i);
+      if(!board.hasAnyRooksConflicts()) {
+        findSolution(row + 1);
+      }
+      board.togglePiece(row,i);
+    }
+  }
 
-  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  findSolution(0);
+
   return solutionCount;
 };
 
 
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
+// return a matrix (an array of arrays) representing a single nxn chessboard, with N queens placed such that none of them can attack each other
+window.findNQueensSolution = function(N) {
   var solution = undefined; //fixme
 
+
+  var board = new Board({n:N})
+  var findSolution = function(row) {
+    if(row === N){
+      solution = board.rows();
+      return;
+    }
+    for(var i = 0; i < N; i++){
+      board.togglePiece(row,i);
+      if(!board.hasAnyRooksConflicts()) {
+        findSolution(row + 1);
+      }
+      board.togglePiece(row,i);
+    }
+  }
+
+  findSolution(0);
   // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+window.countNQueensSolutions = function(N) {
+  var solutionCount = 0; //fixme
 
+  var board = new Board({n:N})
+  var findSolution = function(row) {
+    if(row === N){
+      solutionCount++;
+      return;
+    }
+    for(var i = 0; i < N; i++){
+      board.togglePiece(row,i);
+      if(!board.hasAnyQueensConflicts()) {
+        findSolution(row + 1);
+      }
+      board.togglePiece(row,i);
+    }
+  }
+
+  findSolution(0);
   // console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
